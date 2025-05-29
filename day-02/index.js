3ef899cf462f7c0b1e0f47dfd7ecaa1b04d9278a
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-29 23:40:35 +0600

    learn how to declare varialbe

--- day-02/index.js
+++ day-02/index.js
@@ -1,1 +1,18 @@
 console.log("Learn JavaScript Day - 02");
+
+// What is variable ?
+// -> Variable are  used to store data in JavaScript.
+
+// Variable declaration ?
+
+// keyword : var -
+var number = 10;
+console.log(number); // ans: 10
+
+// keyword : let -
+let personName = "Paul";
+console.log(personName); // ans: Paul
+
+// keyword : const -
+let cityName = "Dhaka";
+console.log(cityName); // Dhaka
