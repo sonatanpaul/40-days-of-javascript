0d7bc088ec73fccdbd015f0165045ce703213a76
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-30 17:18:40 +0600

    problem solve task 2 and task 3

--- day-02/task.js
+++ day-02/task.js
@@ -4,4 +4,26 @@ let name = "Bristi Das";
 let age = 26;
 let isStudent = true;
 
+// ✅ Task 2: Print the values to the console.
+console.log(name); // Bristi Das
+console.log(age); // 26
+console.log(isStudent); // true
 
+// Task 3: Try reassigning values to let and const variables and observe errors.
+// let keyword variables
+name = "No Need GF";
+age = 28;
+isStudent = false;
+// result
+console.log(name); // No Need GF
+console.log(age); // 28
+console.log(isStudent); // False
+
+// const keyword variables
+const village = "Gobindopur";
+const postCode = 6661;
+
+// try re-assigning
+village = "Paul Para"; //  Error
+postCode = 6666; // Error
+// Note : const variable can't re-asign allow .
