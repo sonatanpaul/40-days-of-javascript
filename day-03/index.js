de885380a5a95d673554a04975799b72356ed066
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-30 23:02:03 +0600

    learn assignment operator

--- day-03/index.js
+++ day-03/index.js
@@ -55,3 +55,17 @@ console.log(num1 + num2);
   console.log(count); // 7
   console.log(++count); // 8
 }
+
+// Assignment Operator
+
+let x = 10;
+console.log(x); // 10
+x += 5; //  x = x + 5
+console.log(x); //  15
+x -= 3; //  x = x - 3
+console.log(x); // 12
+x *= 2; // x = x * 2
+console.log(x); // 24
+x /= 4; // x = x / 4
+console.log(x); // 6
+
