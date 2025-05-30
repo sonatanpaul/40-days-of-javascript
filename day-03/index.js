f5beadfd43a47117af5c86a369f835983a1bcd8e
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-30 22:55:51 +0600

    learn pre increment and post increment in javascitp

--- day-03/index.js
+++ day-03/index.js
@@ -1,1 +1,57 @@
-console.log('Hello JavaScript')
+console.log("Hello JavaScript");
+
+// Operator =>  Symobls + - / *
+// Operands =>  x + y ( x and y is operands)
+// Expression  =>  x + y
+// Example : let number = 4 + 5
+
+// Arithmetic Operator :
+// Number Data Type
+let a = 10;
+let b = 20;
+
+console.log(a + b); // 30
+console.log(a - b); // - 10
+console.log(b - a); // 10
+console.log(a * b); // 200
+console.log(a / b);
+
+// String Data Type
+let f_name = "sonatan";
+let l_name = "paul";
+console.log(f_name + l_name); // sonatanpaul
+let num1 = "20";
+let num2 = "30";
+console.log(num1 + num2);
+
+{
+  let a = 2;
+  let b = 3;
+  console.log(a ** b); // 2*2*2 = 8
+}
+
+// Riminder Operator
+{
+  let num1 = 12;
+  let num2 = 5;
+  console.log(num1 % num2); // 2
+}
+
+{
+  // post increment
+  let count = 5;
+  console.log(count); // 5
+  console.log(count++); // count = count + 1
+  console.log(count); // 6
+}
+
+{
+  // pre increment
+  let count = 5;
+  console.log(count); // 5
+  console.log(++count); // 6
+  console.log(count);
+  console.log(count++); // 6
+  console.log(count); // 7
+  console.log(++count); // 8
+}
