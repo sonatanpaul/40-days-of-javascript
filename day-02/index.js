caf6d8344b1c729e9c519f1ea9ac8df6d68f8d99
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-30 17:32:03 +0600

    learn array and object

--- day-02/index.js
+++ day-02/index.js
@@ -16,3 +16,14 @@ console.log(personName); // ans: Paul
 // keyword : const -
 let cityName = "Dhaka";
 console.log(cityName); // Dhaka
+
+// object declaration
+const names = {
+  name: "Paul",
+  age: 26,
+};
+console.log(names); // {name: 'Paul', age: 26}
+
+// array declaration
+const nums = [1, 2, 3, 4, 5, 6];
+console.log(nums); // [1, 2, 3, 4, 5, 6]
