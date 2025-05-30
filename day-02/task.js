3eec752d71d7dab1d505e74681c2198c0fdbcb81
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-30 17:29:39 +0600

    all task problem solve and task added

--- day-02/task.js
+++ day-02/task.js
@@ -24,6 +24,39 @@ const village = "Gobindopur";
 const postCode = 6661;
 
 // try re-assigning
-village = "Paul Para"; //  Error
-postCode = 6666; // Error
+// village = "Paul Para"; //  Error
+// postCode = 6666; // Error
 // Note : const variable can't re-asign allow .
+
+//  Task 4: Create an object and an array, assign them to new variables, modify, and observe changes.
+
+// Object declaration
+let persons = {
+  name: "Paul",
+  age: 26,
+  isLoser: true,
+};
+console.log(persons); // {name: 'Paul', age: 26, isLoser: true}
+
+// assign new variables
+const newPersons = persons;
+console.log(newPersons); // {name: 'Paul', age: 26, isLoser: true}
+
+// modified newPersons value
+newPersons.name = "Sonatan Paul";
+console.log(newPersons); // {name: 'Sonatan Paul', age: 26, isLoser: true}
+console.log(persons); // {name: 'Sonatan Paul', age: 26, isLoser: true}
+
+// Array Declaration :
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers); // [1, 2, 3, 4, 5, 6]
+
+// assign new variable
+const newNumbars = numbers;
+console.log(newNumbars); // [1, 2, 3, 4, 5, 6]
+
+// modified newNumbers value;
+newNumbars[6] = 7;
+console.log(newNumbars); // [1, 2, 3, 4, 5, 6, 7]
+console.log(numbers); // [1, 2, 3, 4, 5, 6, 7]
