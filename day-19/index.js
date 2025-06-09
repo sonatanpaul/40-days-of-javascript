61177b932f6d2dfcdc0916144b2951b5aaf9cfe3
sonatanpaul <iamsonatanpaul@gmail.com>
2025-06-10 02:30:34 +0600

    learn event handling in javaScript

--- day-19/index.js
+++ day-19/index.js
@@ -2,6 +2,21 @@ console.log("Day 19: MASTER JavaScript Events Like a PRO 🤩");
 // What is an Event (Browser)
 // An Event is just a Signal that something happened in the browser.
 
+// Event Handling in Markup 
 function handleClick(message) {
   console.log("Hey, Man", message);
 }
+
+
+// Event Handling is Script : 
+const myBtnElem = document.getElementById("myBtn");
+// Option - 1
+myBtnElem.onclick = function () {
+  console.log("Hello World");
+};
+
+// Option - 2
+function buttonClickAgain() {
+  console.log("Click Me Again");
+}
+myBtnElem.onclick = buttonClickAgain;
