f47ef42415b96e378c86364a2fc1e7509f8a72f1
sonatanpaul <iamsonatanpaul@gmail.com>
2025-06-10 02:18:37 +0600

    learn event handling in markup

--- day-19/index.js
+++ day-19/index.js
@@ -2,3 +2,6 @@ console.log("Day 19: MASTER JavaScript Events Like a PRO 🤩");
 // What is an Event (Browser)
 // An Event is just a Signal that something happened in the browser.
 
+function handleClick(message) {
+  console.log("Hey, Man", message);
+}
